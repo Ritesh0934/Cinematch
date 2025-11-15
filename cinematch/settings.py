77af0eb92db2e72d969403afb99c774cd256803a
Ritesh Pagar <riteshpagar13@gmail.com>
2025-11-15 18:04:03 +0530

    Third commit

--- cinematch/settings.py
+++ cinematch/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-179r4=l-3@)7!^52u)84@1vm@rre_i@v!%1ni3dlwjjgcr8739'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
