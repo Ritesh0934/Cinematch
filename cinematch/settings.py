64035e22bbbd55b1b584001c91d8c17c0131795b
Ritesh Pagar <riteshpagar13@gmail.com>
2025-11-15 18:53:12 +0530

    Sixth commit

--- cinematch/settings.py
+++ cinematch/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
